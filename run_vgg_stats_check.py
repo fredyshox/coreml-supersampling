10df57061d9d9e7cbea2dc56f44d61fdf4b273ff
Kacper Rączy <gfw.kra@gmail.com>
2021-09-12 18:25:53 +0200

    Added support for patches

--- run_vgg_stats_check.py
+++ run_vgg_stats_check.py
@@ -5,6 +5,8 @@ from tqdm import tqdm
 from model.dataset import RGBDMotionDataset
 from model.vgg import PerceptualFPVGG16
 
+UPSAMPLING_FACTOR = 4
+
 
 def online_welford_mean_var(gen):
     n = 0
@@ -20,19 +22,50 @@ def online_welford_mean_var(gen):
     return (n, mean, variance)
 
 
-def main(args):
+def create_dataset(args):
+    patch_size, patch_step = None, None
+    target_size, target_step = None, None
+    if args.patch_step is not None and args.patch_size is not None:
+        patch_size = args.patch_size
+        patch_step = args.patch_step
+        target_size = (
+            patch_size[0] * UPSAMPLING_FACTOR, 
+            patch_size[1] * UPSAMPLING_FACTOR
+        )
+        target_step = (
+            patch_step[0] * UPSAMPLING_FACTOR,
+            patch_step[1] * UPSAMPLING_FACTOR
+        )
     dataset_factory = RGBDMotionDataset(
-        args.data_root_dir, args.data_lr_subdir, args.data_hr_subdir
+        args.data_root_dir, args.data_lr_subdir, args.data_hr_subdir,
+        image_patch_size=args.patch_size, image_patch_step=args.patch_step,
+        target_patch_size=target_size, target_patch_step=target_step
     )
     dataset = dataset_factory.tf_dataset(
         seq_frame_overlap_mode="all", 
         use_keras_input_mapping=True, 
-        create_patches=False
+        create_patches=(patch_size is not None)
     )
     dataset = dataset.batch(args.batch)
     if args.data_limit is not None:
         dataset = dataset.take(args.data_limit)
     dataset = dataset.shuffle(args.buffer_shuffle).prefetch(args.buffer_prefetch)
+
+    return dataset
+
+
+def log_clearml(data):
+    header = ["layer", "n", "mean", "variance", "stardard deviation"]
+    rows = [header] + [list(row) for row in data]
+    clearml.Logger.current_logger().report_table(
+        "VGG activation stats",
+        "n/mean/var/stddev",
+        table_plot=rows
+    )
+
+
+def main(args):
+    dataset = create_dataset(args)
 
     input_element_spec = dataset.element_spec[1]
     input_shape = input_element_spec.shape[-3:]
@@ -56,22 +89,14 @@ def main(args):
         log_clearml(results)
 
 
-def log_clearml(data):
-    header = ["layer", "n", "mean", "variance", "stardard deviation"]
-    rows = [header] + [list(row) for row in data]
-    clearml.Logger.current_logger().report_table(
-        "VGG activation stats",
-        "n/mean/var/stddev",
-        table_plot=rows
-    )
-
-
 def parse_args():
     parser = argparse.ArgumentParser(description="Check dataset distribution")
     parser.add_argument("--data-root-dir", required=True, help="Dataset root dir")
     parser.add_argument("--data-lr-subdir", required=True, help="Dataset low-res subdir")
     parser.add_argument("--data-hr-subdir", required=True, help="Dataset high-res subdir")
     parser.add_argument("--data-limit", default=None, type=int, help="Dataset sample limit")
+    parser.add_argument("--patch-size", default=None, action="store", type=int, nargs=2, help="Image patch size")
+    parser.add_argument("--patch-step", default=None, action="store", type=int, nargs=2, help="Image patch step")
     parser.add_argument("--vgg-layers", required=True, action="store", type=str, nargs="+", help="VGG layers to use in perceptual loss")
     parser.add_argument("--batch", default=1, type=int, help="Batch size")
     parser.add_argument("--buffer-shuffle", default=128, type=int, help="Dataset shuffle buffer size")
