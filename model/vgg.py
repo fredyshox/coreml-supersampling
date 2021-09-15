5dc1f7029ba638cae6f45f3f69409b701a1a615c
Kacper Rączy <gfw.kra@gmail.com>
2021-09-15 23:11:25 +0200

    Added loc/scale cmdline arguments

--- model/vgg.py
+++ model/vgg.py
@@ -1,3 +1,4 @@
+from tensorflow import is_tensor
 from tensorflow.keras.models import Model
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 
@@ -5,10 +6,12 @@ class PerceptualFPVGG16(Model):
     """
     VGG16 wrapper with input in floating-point format (0...1) with activation outputs
     """
-    def __init__(self, weights, input_shape, output_layer_names, trainable=False):
+    def __init__(self, weights, input_shape, output_layer_names, loc, scale, trainable=False):
         super().__init__()
         
         vgg_model = VGG16(include_top=False, weights=weights, input_shape=input_shape)
+        self.loc = loc # mean
+        self.scale = scale # stddev
         self.custom_vgg_model = Model(vgg_model.input, [vgg_model.get_layer(layer).output for layer in output_layer_names])
         self.custom_vgg_model.trainable = trainable
         self.trainable = trainable
@@ -20,6 +23,9 @@ class PerceptualFPVGG16(Model):
         scaled_x = x * 255.0
         preprocessed_x = preprocess_input(scaled_x)
         output = self.custom_vgg_model(preprocessed_x)
-        standardized_output = (output - 90.10304260253906) / 40.430442810058594
+        if is_tensor(output):
+            standardized_output = (output - self.loc) / self.scale
+        else: # must be list then (multi output)
+            standardized_output = [(activation - self.loc) / self.scale for activation in output]
 
         return standardized_output
