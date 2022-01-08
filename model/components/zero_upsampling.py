8dd3b28afaae071d3931375b7cfb2a24ca5b4615
Kacper Rączy <gfw.kra@gmail.com>
2022-01-08 22:52:38 +0100

    Fixed zero upsampling with new implementation

--- model/components/zero_upsampling.py
+++ model/components/zero_upsampling.py
@@ -1,13 +1,25 @@
 import tensorflow as tf
 
+
+def _get_dim(x, idx):
+    if x.shape.ndims is None:
+        return tf.shape(x)[idx]
+    return x.shape[idx] or tf.shape(x)[idx]
+
+
 class ZeroUpsampling(tf.Module):
     def __init__(self, scale_factor, name=None):
         super().__init__(name=name)
         self.scale_factor = scale_factor
-        self.scale_mat = tf.constant([1, scale_factor, scale_factor, 1], dtype=tf.int32)
 
     def __call__(self, x):
-        input_shape = tf.shape(x)
-        kernel = tf.ones([1, 1, input_shape[-1], input_shape[-1]])
-        output_shape = input_shape * self.scale_mat
-        return tf.nn.conv2d_transpose(x, kernel, output_shape, self.scale_factor, padding="VALID")
+        height, width, channels = (
+            _get_dim(x, 1),
+            _get_dim(x, 2),
+            _get_dim(x, 3),
+        )
+
+        grid_x, grid_y = tf.meshgrid(tf.range(width), tf.range(height))
+        doubled_grid_yx = tf.stack([grid_y, grid_x], axis=2)*2
+        output_shape = tf.constant([height*self.scale_factor, width*self.scale_factor, channels])
+        return tf.map_fn(lambda i: tf.scatter_nd(doubled_grid_yx, i, output_shape), elems=x)
